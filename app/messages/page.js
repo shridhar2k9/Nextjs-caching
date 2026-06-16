17905f9afb53fc91334c7cd48e0e4e79285e245e
shridhar2k9 <shridhar2k9@gmail.com>
2026-06-16 07:32:29 +0530

    added revalidate congif in fetch to restrict duplicate api request

--- app/messages/page.js
+++ app/messages/page.js
@@ -4,7 +4,11 @@ export default async function MessagesPage() {
   // this two option used to cache and to not caching config using fetch method
   const response = await fetch('http://localhost:8080/messages', {
     // cache:'force-cache',
-    cache:'no-store'
+    // cache:'no-store',
+    // now after initial load it wont request this api call for 5sec
+    next:{
+      revalidate:5
+    }
   });
   const messages = await response.json();
 
