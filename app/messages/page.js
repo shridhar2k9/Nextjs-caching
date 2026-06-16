c2b7a8fc06735a5fb7370d361f1ef1e44786dac7
shridhar2k9 <shridhar2k9@gmail.com>
2026-06-16 07:39:50 +0530

    replaced revalidate and no-store using reserved constants revalidate and dynamic - ISR(incrementel static regeneration)

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,14 +1,18 @@
 import Messages from '@/components/messages';
 
+// Incremental static Regeneration (ISR)
+// const revalidate = 5;
+const dynamic = "no-store"
+
 export default async function MessagesPage() {
   // this two option used to cache and to not caching config using fetch method
   const response = await fetch('http://localhost:8080/messages', {
     // cache:'force-cache',
     // cache:'no-store',
     // now after initial load it wont request this api call for 5sec
-    next:{
-      revalidate:5
-    }
+    // next:{
+    //   revalidate:5
+    // }
   });
   const messages = await response.json();
 
