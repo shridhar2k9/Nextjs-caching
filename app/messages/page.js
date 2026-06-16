ee375930b6d70bc4675c5f2ab5385b22acef4795
shridhar2k9 <shridhar2k9@gmail.com>
2026-06-16 07:29:06 +0530

    caching config in fetch request

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,10 +1,10 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
+  // this two option used to cache and to not caching config using fetch method
   const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
+    // cache:'force-cache',
+    cache:'no-store'
   });
   const messages = await response.json();
 
